93bb7cd25f05411ea6e4a2ecfad1d5bd7a6192c4
Myk3le <myk3le@proton.me>
2025-09-03 11:33:12 -0700

    Initial commit

---
+++ Coach-App/src/pages/requests/RequestsReceived.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <base-dialog :show="!!error" title="An Error occured!" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section>
+      <base-card>
+        <header>
+          <h2>Requests Received</h2>
+        </header>
+
+        <base-spinner v-if="isLoading"></base-spinner>
+
+        <ul v-else-if="hasRequests && !isLoading">
+          <request-item
+            v-for="req in receivedRequests"
+            :key="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+          ></request-item>
+        </ul>
+
+        <h3 v-else>You haven't received any requests yet!</h3>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+  created() {
+    this.loadRequests();
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  methods: {
+    handleError() {
+      this.error = null;
+    },
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed';
+      }
+      this.isLoading = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
